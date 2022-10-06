5587a10d8b9c623e4b31ee303dcb46ac0ced94e3
juperameza <juperameza@hotmail.com>
2022-10-06 03:39:18 -0500

    Starting with modals

--- db/schema.rb
+++ db/schema.rb
@@ -33,22 +33,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_174627) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "names", force: :cascade do |t|
-    t.string "LastName"
-    t.string "Sex"
-    t.date "birthDate"
-    t.string "email"
-    t.bigint "phone"
-    t.string "state"
-    t.string "city"
-    t.string "colony"
-    t.string "street"
-    t.string "houseNumber"
-    t.bigint "cp"
-    t.bigint "puntos"
-    t.boolean "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
