5540c51b8d393d2d693fa4d98b3902945f5f3047
juperameza <juperameza@hotmail.com>
2022-10-08 20:20:29 -0500

    Minor changes :-)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_001433) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_09_011722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_001433) do
     t.datetime "updated_at", null: false
     t.integer "salary"
     t.bigint "phone"
+    t.string "email"
   end
 
   create_table "products", force: :cascade do |t|
