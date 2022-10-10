9791fc957a20fcee24ead89fe01487da5a3562fb
juperameza <juperameza@hotmail.com>
2022-10-09 19:53:07 -0500

    Update strong paramUpdate strong paramss

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -65,6 +65,6 @@ class EmployeesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def employee_params
-      params.require(:employee).permit(:name, :last_name, :sex, :position, :shift, :status, :salary)
+      params.require(:employee).permit(:name, :last_name, :sex, :position, :shift, :status, :salary, :phone, :email)
     end
 end
